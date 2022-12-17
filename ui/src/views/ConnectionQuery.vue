7fe7bea8620d0b508f257715e9b99efc3c7844c7
Flawid DSouza <me@flawiddsouza.com>
2022-12-17 15:28:34 +0530

    query view > run multiple queries at once

--- ui/src/views/ConnectionQuery.vue
+++ ui/src/views/ConnectionQuery.vue
@@ -1,25 +1,27 @@
 <template>
     <h2>Query</h2>
 
-    <div class="message success mb-2" v-if="queryRan">
-        <template v-if="rows.length === 0">No rows</template>
-        <template v-else>{{ new Intl.NumberFormat().format(rows.length) }} {{ rows.length > 1 ? 'rows' : 'row' }} returned</template>
-    </div>
+    <template v-for="queryRun in queriesRun">
+        <div class="message success mb-2" v-if="queryRun.queryRan">
+            <template v-if="queryRun.rows.length === 0">No rows</template>
+            <template v-else>{{ new Intl.NumberFormat().format(queryRun.rows.length) }} {{ queryRun.rows.length > 1 ? 'rows' : 'row' }} returned</template>
+        </div>
 
-    <div class="message error mb-2" v-if="error">{{ error }}</div>
+        <div class="message error mb-2" v-if="queryRun.error">{{ queryRun.error }}</div>
 
-    <table class="mb-2" v-if="rows.length > 0">
-        <thead>
-            <tr>
-                <th v-for="rowHeader in rowHeaders">{{ rowHeader }}</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="row in rows">
-                <td v-for="rowHeader in rowHeaders">{{ row[rowHeader] }}</td>
-            </tr>
-        </tbody>
-    </table>
+        <table class="mb-2" v-if="queryRun.rows.length > 0">
+            <thead>
+                <tr>
+                    <th v-for="rowHeader in queryRun.rowHeaders">{{ rowHeader }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="row in queryRun.rows">
+                    <td v-for="rowHeader in queryRun.rowHeaders">{{ row[rowHeader] }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </template>
 
     <textarea style="width: 531px; height: 313px; padding: 3px;" v-model="query"></textarea>
 
@@ -36,32 +38,44 @@ import { addQueryParamsToRoute } from '../libs/helpers.js'
 
 const route = useRoute()
 const query = ref('')
-const rowHeaders = ref([])
-const rows = ref([])
-const error = ref('')
-const queryRan = ref(false)
+const queriesRun = ref([])
 
 async function runQuery() {
-    rows.value = []
-    rowHeaders.value = []
-    error.value = ''
-
     if(route.query.sql !== query.value) {
         addQueryParamsToRoute(route, {
             sql: query.value
         })
     }
 
-    const { success, data } = await api.runQuery(route.params.connectionId, query.value)
+    queriesRun.value = []
 
-    if(success) {
-        rows.value = data
-        rowHeaders.value = rows.value.length > 0 ? Object.keys(rows.value[0]) : []
-        queryRan.value = true
-    } else {
-        error.value = data
-        queryRan.value = false
-    }
+    const queriesToRun = query.value.split(';').filter(item => item)
+
+    const result = await Promise.all(queriesToRun.map(queryToRun => {
+        return api.runQuery(route.params.connectionId, queryToRun)
+    }))
+
+    result.forEach(resultItem => {
+        const { success, data } = resultItem
+
+        const queryRun = {
+            rows: [],
+            rowHeaders: [],
+            error: '',
+            queryRan: false
+        }
+
+        if(success) {
+            queryRun.rows = data
+            queryRun.rowHeaders = queryRun.rows.length > 0 ? Object.keys(queryRun.rows[0]) : []
+            queryRun.queryRan = true
+        } else {
+            queryRun.error = data
+            queryRun.queryRan = false
+        }
+
+        queriesRun.value.push(queryRun)
+    })
 }
 
 onBeforeMount(() => {
