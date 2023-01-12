1477b539422dcfdf838e21690bd9199b856d9bba
Flawid DSouza <me@flawiddsouza.com>
2023-01-12 19:20:26 +0530

    fix: table structure view > for postgres dbs, seeing indexes and foreign keys from multiple schemas - limit to connection schema

--- connection.js
+++ connection.js
@@ -148,7 +148,7 @@ export async function getTableDetails(connectionId, database, tableName) {
                     indexdef as column,
                     indexname as name
                 FROM pg_indexes
-                WHERE tablename = ${tableName};
+                WHERE schemaname = ${connection.schema} AND tablename = ${tableName};
             `
 
             foreignKeys = await sql`
@@ -161,7 +161,7 @@ export async function getTableDetails(connectionId, database, tableName) {
                     information_schema.table_constraints AS tc
                 JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema
                 JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name AND ccu.table_schema = tc.table_schema
-                WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_name = ${tableName};
+                WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_schema = ${connection.schema} AND tc.table_name = ${tableName};
             `
         }
 
